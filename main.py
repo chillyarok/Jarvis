7afc2dc53227640769f75edda32d2ce6e92344cf
chillyarok <122738094+chillyarok@users.noreply.github.com>
2024-02-27 10:30:29 +0300

    new file:   commands/time/whattime.py 	modified:   main.py

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ import stt
 import commands.xmldecoder as speak
 import wake_word_detect as wwd
 import random
-
+import time
 
 
 print("good")
@@ -11,7 +11,11 @@ while True:
     if wwd.WakeWordDetect() == True:
         print(1)
         for text in stt.listen():
+            t = time.time()
             print(text)
             if text in str(speak.speakkommand().keys()):
+                t = time.time()
                 textf = speak.speakkommand()[text][random.randint(0,len(speak.speakkommand()[text])-1)]
                 tts.say(textf)
+            if time.time()-t>15:
+                break
