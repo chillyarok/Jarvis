6e3943afcd6b453ae66ada6216ed1994559e21dc
chillyarok <122738094+chillyarok@users.noreply.github.com>
2024-02-28 19:22:54 +0300

    modified:   main.py

--- main.py
+++ main.py
@@ -5,29 +5,23 @@ import commands.time.whattime as wtime
 import wake_word_detect as wwd
 import commands.weather.weather as weather
 import random
-import time
 
 
 print("good")
 while True:
     if wwd.WakeWordDetect() == True:
         tts.say("слушаю.")
-        t = time.time()
         for text in stt.listen():      
             print(text)
             if text in str(speak.speakkommand().keys()):
-                t = time.time()
                 textf = speak.speakkommand()[text][random.randint(0,len(speak.speakkommand()[text])-1)]
                 tts.say(textf)
             if speak.timedecoder("date") in text:
-                t = time.time()
                 textf = wtime.howdate()
                 tts.say(textf)
             if speak.timedecoder("time") in text:
-                  t = time.time()
                   textf = wtime.howtime()
                   tts.say(textf)
             if "какая сейчас погода" in text:
-                t = time.time()
                 textf = weather.howweather()
                 tts.say(textf)
