b84ab3eade9da126ddfe743ec29e5f9aca902620
chillyarok <122738094+chillyarok@users.noreply.github.com>
2024-03-02 17:56:14 +0300

    renamed:    commands/time/timeanswers.xml -> commands/ttime/timeanswers.xml 	renamed:    commands/time/ttime.py -> commands/ttime/ttime.py 	modified:   main.py

--- main.py
+++ main.py
@@ -1,36 +1,43 @@
 import tts
 import stt
 import commands.xmldecoder as speak
-import commands.time.ttime as wtime
+import commands.ttime.ttime as wtime
 import wake_word_detect as wwd
 import commands.weather.weather as weather
 import random
-
+import time
 
 print("good")
 while True:
     if wwd.WakeWordDetect() == True:
         tts.say("слушаю.")
+        t = time.time()
         for text in stt.listen():      
             print(text)
             if text in str(speak.speakkommand().keys()):
+                t = time.time()
                 textf = speak.speakkommand()[text][random.randint(0,len(speak.speakkommand()[text])-1)]
                 tts.say(textf)
             for k in speak.timedecoder("date"):
                 if k in text:
+                    t = time.time()
                     textf = wtime.howdate()
                     tts.say(textf)
                 else:
                     continue
             for k in speak.timedecoder("time"):
                 if k in text:
+                    t = time.time()
                     textf = wtime.howtime()
                     tts.say(textf)
                 else:
                     continue
             for k in weather.weatherdecoder():
                 if k in text:
+                    t = time.time()
                     textf = weather.howweather()
                     tts.say(textf)
                 else:
                     continue
+            if time.time()-t>=30:
+                break
